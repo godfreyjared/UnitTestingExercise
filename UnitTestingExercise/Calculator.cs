9eb2c27f808470a1abdc9d9d35f84db9fac69b6d
Jared Godfrey <godfreyjared@gmail.com>
2025-08-21 11:34:25 -0600

    Fixed errors

--- UnitTestingExercise/Calculator.cs
+++ UnitTestingExercise/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 namespace UnitTestingExercise
 {
     public class Calculator
@@ -11,19 +12,23 @@ namespace UnitTestingExercise
 
 
         // Step 2: Create an Add method that accepts 3 integers, adds those integers, and returns an integer
-        //For Example:
+        //For Example: DONE
         public int Add(int num1, int num2, int num3)
         {
             return num1 + num2 + num3;
         }
 
 
-        // Step 3: Navigate to the UnitTests.cs file and complete the AddTest unit test method 
+        // Step 3: Navigate to the UnitTests.cs file and complete the AddTest unit test method DONE
 
 
         // Step 4: 
-        // Create a Subtract method that accepts 2 integers
-        // Keep track of which number is getting passed as minuend and subtrahend
+        // Create a Subtract method that accepts 2 integers DONE
+        // Keep track of which number is getting passed as minuend and subtrahend DONE
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
 
 
         // Step 5: Navigate to the UnitTests.cs file and complete the SubtractTest unit test method
@@ -31,6 +36,10 @@ namespace UnitTestingExercise
 
         // Step 6: 
         // Create a Multiply method that passes 2 integers
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
 
 
         // Step 7: Navigate to the UnitTests.cs file and complete the MultiplyTest unit test method 
@@ -38,6 +47,11 @@ namespace UnitTestingExercise
 
         // Step 8: 
         // Create a Divide method that passes 2 integers
+        public int Divide(int num1, int num2)
+        {
+            if (num2 == 0) throw new DivideByZeroException("Cannot divide by zero");
+            return num1 / num2;
+        }
 
         // Step 9: Navigate to the UnitTests.cs file and complete the DivideTest unit test method 
 
